1905d809899068edf3ea7fb24797620763a79718
Frankie Liu <frankie@frankie-MS-7846.(none)>
2014-02-21 19:38:11 -0800

    Basic frame setup

--- P3/compress.cpp
+++ P3/compress.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 
+int read_input_file(char*,char**);
+int write_output_file(char*,char*,int);
 
 int main(int argc, char** argv){
 
@@ -10,51 +12,66 @@ int main(int argc, char** argv){
 
 	// Initialize Variables
 	int fileSize;
-	char * inputFile;
+	int byteWritten;
+	char* fileContent;
 
 
+	// Read input file and store content in fileContent.
+	fileSize = read_input_file(argv[1], &fileContent);
+	//if(fileSize < 0) return -1;
 
-	// ----------
-	// Read input file into "inputFile" char array
-	// Then close the file.
-	// Open the given file in binary mode
-	ifstream fileIn;
-	fileIn.open(argv[1], ios::ate|ios::binary);
-	if(!fileIn.is_open()){
-		cout << "Error!!" << endl;
-		return 2;
-	} 
-
-	cout << "Good here" << endl;
-	// Get file size first
-	fileSize = fileIn.tellg();
-	inputFile = new char[fileSize];
-	fileIn.seekg(0, ios::beg);
-
-	// Read in the entire file at once
-	fileIn.read(inputFile, fileSize);
-	fileIn.close();
+	// Compress the content using Huffman Coding Tree
 
 
-	cout << inputFile << endl;
+	// Write the compressed content to output file
+	byteWritten = write_output_file(argv[2],fileContent,fileSize);
+	if(byteWritten < 0) return -1;
 
-	// // Write decoded bytes to the given file 
-	// ofstream fileOut;
-	// fileOut.open(argv[2], ios::binary);
-	// if(!fileOut.is_open()) return 3;
-	// // Write to file
-	// char * abc = "this is my file";
-	// fileOut.write(abc, fileSize);
-	// fileOut.close();
-
-
-
+	cout << fileSize << " bytes written to file: " << argv[2] << endl;
 
 	// Clean up
-	delete [] inputFile;
+	delete [] fileContent;
 
 	return 0;
 }
 
 
 //---------------------------------------------------------
+
+// Parameter 1: input file name 					char array
+// Parameter 2: memory address to store content 	address of char array
+int read_input_file(char * fileName, char ** content){
+	ifstream fileIn;
+	fileIn.open(fileName, ios::ate|ios::binary);
+	if(!fileIn.is_open()){
+		cout << "Input file opening Error!!" << endl;
+		delete [] content;
+		return -1;
+	} 
+	// Get file size first
+	int fileSize = fileIn.tellg();
+	*content = new char[fileSize];
+	fileIn.seekg(0, ios::beg);
+	// Read in the entire file at once
+	fileIn.read(*content, fileSize);
+	fileIn.close();
+	return fileSize;
+}
+
+// Parameter 1: output file name 		char array
+// Parameter 2: the content to write	char array
+// Parameter 3: the size of content 	int
+int write_output_file(char* fileName, char* content, int fileSize){
+	// Write decoded bytes to the given file 
+	ofstream fileOut;
+	fileOut.open(fileName, ios::binary);
+	if(!fileOut.is_open()) {
+		cout << "Output file opening Error!!!" << endl;
+		delete [] content;
+		return -1;
+	}
+	// Write to file
+	fileOut.write(content, fileSize);
+	fileOut.close();
+	return fileSize;
+}
